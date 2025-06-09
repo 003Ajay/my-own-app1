419cab4561babcaf4310130250f41711e109afba
003Ajay <ajaya17050@gmail.com>
2025-06-09 09:42:36 +0530

    Initial commit

---
+++ src/pages/about.js
@@ -0,0 +1,3 @@
+export default function About() {
+  return <h1>About Page - Learn more about us!</h1>;
+}
---
+++ src/pages/index.js
@@ -0,0 +1,14 @@
+import Link from 'next/link';
+
+export default function Home() {
+  return (
+    <div>
+      <h1>Welcome to My Next.js App</h1>
+      <nav>
+        <Link href="/about">About</Link> |{" "}
+        <Link href="/contact">Contact</Link> |{" "}
+        <Link href="/services">Services</Link>
+      </nav>
+    </div>
+  );
+}
