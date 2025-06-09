0dde195613654b45b57b16c79145e75505a70561
003Ajay <ajaya17050@gmail.com>
2025-06-09 10:08:46 +0530

    commit

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,6 +4,7 @@ export default function Home() {
   return (
     <div>
       <h1>Welcome to My Next.js App</h1>
+      <h1>Welcome to My Next.js App</h1>
       <nav>
         <Link href="/about">About</Link> |{" "}
         <Link href="/contact">Contact</Link> |{" "}
